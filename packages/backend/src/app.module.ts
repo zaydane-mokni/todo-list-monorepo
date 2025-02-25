d073788bd4f651482a8e15fcf01918cb37414b87
zmokni <zaydane95@gmail.com>
2025-02-25 11:05:12 +0100

    :wastebasket: clean modules

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/todo-list-app'), TasksModule],
 })
 export class AppModule {}
