2aac3de34c810a3875b00260a259de97b9daa9e5
zmokni <zaydane95@gmail.com>
2025-02-26 22:28:30 +0100

    :wrench: vite conf

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 });
